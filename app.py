8ccd74ffdcc6bb4e631f1ec172dc6ae43a8a8d3f
janguuon\janguuon <janguuon@gmail.com>
2024-11-07 00:00:05 +0900

    - 즈므즈므 웹 개발 시작

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import psycopg2
+import pandas as pd
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(host='localhost', dbname='mydb',user='postgres', password=1234, port='5432')
+
+#커서 만들기
+#커서 객체는 실질적으로 DB에 쿼리문을 수행하고, 결과를 가져오는 역할
+cur = conn.cursor()
+
+#쿼리문 수행
+#cur.execute('SELECT * FROM public.f1_fasoo')
+#info = cur.fetchall()
+
+cur.close()
+conn.close()
+
+@app.route('/')
+def render_home():
+    return render_template('index.html')
+
+@app.route('/book')
+def render_book():
+    return '예약하기'
+
+@app.route('/book/check')
+def render_book_check():
+    return '결제하기'
+
+if __name__ == '__main__':
+    app.run(debug=True)
