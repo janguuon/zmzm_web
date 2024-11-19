972212906e4f065cb909de17f58b5437535c97c8
janguuon\janguuon <janguuon@gmail.com>
2024-11-20 00:29:13 +0900

    - 로그인 기능 구현중

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from Routes import Login
 import psycopg2
 import pandas as pd
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(host='localhost', dbname='mydb',user='postgres', password=1234, port='5432')
+#conn = psycopg2.connect(host='localhost', dbname='mydb',user='postgres', password=1234, port='5432')
 
 #커서 만들기
 #커서 객체는 실질적으로 DB에 쿼리문을 수행하고, 결과를 가져오는 역할
-cur = conn.cursor()
+#cur = conn.cursor()
 
 #쿼리문 수행
 #cur.execute('SELECT * FROM public.f1_fasoo')
 #info = cur.fetchall()
 
-cur.close()
-conn.close()
+#cur.close()
+#conn.close()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def render_home():
+    if request.method == 'POST':
+        username = request.form['userName']
+        username = str(username)
+        password = request.form['userPassword']
+        password = str(password)
+        print(username + password)
     return render_template('index.html')
 
 @app.route('/book')
