0156361d0ef9602f042b9bcf1fabb64335647896
OhJangWon <ojw.sat@gmail.com>
2024-12-02 12:41:34 +0900

    - 웹 페이지 이동 작업중

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from fastapi .staticfiles import StaticFiles
 from routers import index
 app = FastAPI()
 
-#app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(index.router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("app:app", host='0.0.0.0', port=5000, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
--- routers/index.py
+++ routers/index.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, APIRouter, Request
 from fastapi .templating import Jinja2Templates
 from fastapi .responses import HTMLResponse
-import os
 
 router = APIRouter()
 app = FastAPI()
@@ -12,20 +11,6 @@ templates = Jinja2Templates(directory="templates")
 async def index_Page(request: Request):
     return templates.TemplateResponse("index.html", context={"request":request})
 
-@router.post("/dir/test")
-async def test():
-    print("시작")
-    text='test시작'
-    return text
-
-# bp = Blueprint('home', __name__)
-
-# @bp.route('/', methods=['GET', 'POST'])
-# def render_home():
-#     if request.method == 'POST':
-#         username = request.form['userName']
-#         username = str(username)
-#         password = request.form['userPassword']
-#         password = str(password)
-#         print(username + " " + password)
-#     return render_template('index.html')
+@router.get("/login", response_class=HTMLResponse)
+async def login_Page(request: Request):
+    return templates.TemplateResponse("login.html", context={"request":request})
